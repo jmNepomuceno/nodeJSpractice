ee27ddd80d80e9f8ac1a6986c8184720f8faf659
toastDerivation <jmgnngmj@gmail.com>
2021-07-16 19:19:00 +0800

    71621_b

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express')
+const {students} = require('./sortDatabase')
 const app = express()
 
 app.use(express.static(__dirname + '/views'));
 app.set('view engine','ejs');
 
-app.get('/', (req,res)=>{
-    res.render('sort')
-})
+function compare( a, b ) {
+    if ( a.id < b.id ){
+      return -1;
+    }
+    if ( a.id > b.id ){
+      return 1;
+    }
+    return 0;
+}
 
+app.get('/', (req,res)=>{
+    const {from} = req.query
+    const {end} = req.query
+    let temp_students = students
+    temp_students.sort(compare);
+    res.render('sort',{
+        from : from,
+        end : end,
+        db : temp_students
+    })
+})
+  
 const port = process.env.PORT || 3000
 app.listen(port , ()=> console.log(`Listening to port ${port}`))
